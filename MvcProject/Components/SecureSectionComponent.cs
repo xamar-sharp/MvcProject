0415774de9ddba3df18bc13b7dd15b627841204b
xamar-sharp <xamacoredevelopment@gmail.com>
2022-05-14 22:58:40 +0300

    added components

---
+++ MvcProject/Components/SecureSectionComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+namespace MvcProject.Components
+{
+    public class SecureSectionComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(string viewName,string html=null)
+        {
+            if(html is not null)
+            {
+                return new HtmlContentViewComponentResult(new HtmlString(html));
+            }
+            else
+            {
+                return View(viewName);
+            }
+        }
+    }
+}
